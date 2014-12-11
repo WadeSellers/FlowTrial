7997633bb72f9d30b85c438d00bdfb85ae065008
Wade Sellers <Waderoni@gmail.com>
2014-12-10 20:47:55 -0700

    Initial Commit

---
+++ FlowTrial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlowTrial
+//
+//  Created by Wade Sellers on 12/10/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
